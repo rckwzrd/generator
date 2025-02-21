ac0b99291fdcf008a8d48d2fca51337c7e5d9e51
matt rhoads <mrhoads1@gmail.com>
2025-02-21 16:32:26 -0600

    added block type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,2 +1,45 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+
+def block_to_type(block):
+    lines = block.split("\n")
+    if block.startswith(("#", "##", "###", "####", "#####", "######")):
+        return BlockType.HEADING
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if block.startswith("> "):
+        for line in lines:
+            if not line.startswith("> "):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    return BlockType.PARAGRAPH
+
+
 def markdown_to_blocks(markdown):
     return [block.strip() for block in markdown.split("\n\n") if block != ""]
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_type, BlockType
 
 test_md = """# This is a heading
 
@@ -8,6 +8,10 @@ This is a paragraph.\n It has **bold** and *italic* words.
 * item
 * item
 * item
+
+```
+code block
+```
 
 """
 
@@ -31,13 +35,32 @@ This is paragraph 2.
 """
 
 
+class TestBlockTypes(unittest.TestCase):
+    def test_block_types(self):
+        h = "# heading 1"
+        self.assertEqual(block_to_type(h), BlockType.HEADING)
+        ul_star = "* item\n* item\n* item"
+        self.assertEqual(block_to_type(ul_star), BlockType.UNORDERED_LIST)
+        ul_dash = "- item\n- item\n- item"
+        self.assertEqual(block_to_type(ul_dash), BlockType.UNORDERED_LIST)
+        li = "1. item\n2. item\n3. item"
+        self.assertEqual(block_to_type(li), BlockType.ORDERED_LIST)
+        code = "```\ncode block\n```"
+        self.assertEqual(block_to_type(code), BlockType.CODE)
+        quote = "> word quotes\n> more quotes"
+        self.assertEqual(block_to_type(quote), BlockType.QUOTE)
+        p = "This is a paragraph.\nWith **words**."
+        self.assertEqual(block_to_type(p), BlockType.PARAGRAPH)
+
+
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
         h1 = "# This is a heading"
-        p = "This is a paragraph. It has **bold** and *italic* words."
+        p = "This is a paragraph.\n It has **bold** and *italic* words."
         li = "* item\n* item\n* item"
+        code = "```\ncode block\n```"
         blocks = markdown_to_blocks(test_md)
-        case = [h1, p, li]
+        case = [h1, p, li, code]
         self.assertListEqual(blocks, case)
 
     def test_multi_newline(self):
